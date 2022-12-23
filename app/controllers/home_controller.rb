08365381593ef387e869ee4c76d821b8b2e0bc6f
Amaar Fazlani <fazlaniamaar@gmail.com>
2022-12-23 17:44:12 -0600

    Add author and published at to home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,28 @@ class HomeController < ApplicationController
     response = HTTParty.get("http://www.loc.gov/items?fo=json&fa=original-format:book&q=#{params[:q]}")
 
     @results = response['results'].map do |result|
-      { 
+      {
         loc_id: result['id'],
         image_url:  result['image_url'][0],
         title: result['title'],
-        link_to_loc: result['url']
+        link_to_loc: result['url'],
+        contributors: parse_contributers(result['item']) ,
+        published_at: parse_published_at(result['item'])
       }
     end
+  end
+
+  private
+
+  def parse_contributers(item)
+    return "N/A" if item.nil? || item['contributors'].nil?
+    
+    item['contributors'].join(", ")
+  end
+
+  def parse_published_at(item)
+    return "N/A" if item.nil? || item['created_published'].nil?
+
+    item['created_published'].join(", ")
   end
 end
