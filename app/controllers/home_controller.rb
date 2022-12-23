154cdf01f5c5d368a8e76f2349444a35c6357dc8
Amaar Fazlani <fazlaniamaar@gmail.com>
2022-12-23 10:43:25 -0600

    Finsih adding search form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,14 @@
 class HomeController < ApplicationController
   def index
-    response = HTTParty.get("http://www.loc.gov/items?fo=json&fa=original-format:book")
+    response = HTTParty.get("http://www.loc.gov/items?fo=json&fa=original-format:book&q=#{params[:q]}")
 
     @results = response['results'].map do |result|
       { 
         loc_id: result['id'],
         image_url:  result['image_url'][0],
         title: result['title'],
-        link_to_loc: result['url'],
-        resources: result['resources']
+        link_to_loc: result['url']
       }
     end
-    
   end
 end
